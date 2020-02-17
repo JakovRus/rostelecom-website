2e4ca32c4ab715512ad87a06c1fe6bf45e15d24b
Yakov Rogov <rogov@haulmont.com>
2020-02-17 16:27:37 +0400

    added success message

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -5,21 +5,53 @@ import {RWButton} from "../button/button";
 import styles from './contact-from.module.scss';
 
 export function ContactForm() {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
+  const [name, _setName] = useState('');
+  const [phone, _setPhone] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
-  console.log(phone);
+  const setName = (event) => {
+    const {value} = event.target;
+    _setName(value);
+  };
+
+  const setPhone = (event) => {
+    const {value} = event.target;
+    _setPhone(value);
+  };
+
+  const submit = () => {
+    setSubmitted(true);
+  };
 
   return (
     <div className={styles.form}>
-      <RWInput value={name} onChangeText={setName}
-               placeholder='Имя' containerClass={styles.formElement}/>
-      <RWPhoneInput value={phone} onChangeText={setPhone}
-                    placeholder='Номер телефона'
-                    containerClass={styles.formElement}/>
-      <RWButton className={styles.formElement}>
-        Подключить
-      </RWButton>
+      {
+        !submitted ? (
+          <Form name={name} setName={setName}
+                phone={phone} setPhone={setPhone}
+                submit={submit} />
+        ) : (
+          <span className={styles.successMessage}>
+            Ваша заявка принята! Наш сотрудник скоро перезвонит Вам.
+          </span>
+        )
+      }
     </div>
   );
+}
+
+function Form(props) {
+  return (
+    <>
+      <RWInput value={props.name} onChange={props.setName}
+               placeholder='Имя' containerClass={styles.formElement}/>
+      <RWPhoneInput value={props.phone} onChange={props.setPhone}
+                    placeholder='Номер телефона'
+                    containerClass={styles.formElement}/>
+      <RWButton className={styles.formElement}
+                onClick={props.submit}>
+        Подключить
+      </RWButton>
+    </>
+  )
 }
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -6,10 +6,16 @@ import {CloseButton} from "../close-button/close-button";
 export function Modal({children, visible, close}) {
   const className = visible ? styles.modal : styles.modal_hidden;
 
+  const preventClose = (event) => {
+    event.stopPropagation();
+  };
+
   return (
     <div className={className}>
       <div onClick={close} className={styles.content}>
+        <div onClick={preventClose}>
         {children}
+        </div>
         <CloseButton />
       </div>
     </div>
