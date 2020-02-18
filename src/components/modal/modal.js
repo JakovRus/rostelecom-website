20c907828bfd4bce6eb68b77221ba71ee9f97bd8
Yakov Rogov <rogov@haulmont.com>
2020-02-18 18:44:22 +0400

    added 404 page

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,15 +3,16 @@ import React from 'react';
 import styles from './modal.module.scss';
 import {CloseButton} from "../close-button/close-button";
 
-export function Modal({children, visible, close}) {
+export function Modal({children, visible, closeButtonHidden}) {
   const className = visible ? styles.modal : styles.modal_hidden;
-
 
   return (
     <div className={className}>
       <div className={styles.content}>
         {children}
-        <CloseButton />
+        {
+          !closeButtonHidden && <CloseButton />
+        }
       </div>
     </div>
   );
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,32 @@ import React from 'react';
 
 import {Layout} from '../components/layout/layout';
 import SEO from '../components/seo';
+import {Link} from 'gatsby';
+import {Modal} from '../components/modal/modal';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found"/>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 - Не найдено"/>
+    <Modal visible closeButtonHidden>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        <h1 style={{
+          textAlign: "center",
+        }}>
+          Хм, кажется, здесь что-то было...
+        </h1>
+        <p style={{
+          textAlign: "center",
+        }}>
+          Но, похоже, мы не можем найти то, что вам нужно.
+        </p>
+        <Link to={"/"}>Вернуться на главную</Link>
+      </div>
+    </Modal>
   </Layout>
 );
 
